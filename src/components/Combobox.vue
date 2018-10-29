8c5afb35cb5b685358bb2935c120cf261cada2c3
Hardin <569011@bah.com>
2018-10-29 07:38:55 -0400

    combobox styling (fixed overflow styling).

--- src/components/Combobox.vue
+++ src/components/Combobox.vue
@@ -254,6 +254,7 @@ svg.dropdown.active {
 }
 .combobox-container {
   position: relative;
+  width: 100%;
 }
 .combobox {
   box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12);
